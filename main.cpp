fed550f67b05001794e16c65bcca604d2ef87808
sirivan <vanyaturianitsa@gmail.com>
2016-04-20 17:21:39 +0200

    use as servo methods -lock -unlock -switch

--- main.cpp
+++ main.cpp
@@ -11,15 +11,8 @@ private:
     int angle2 = 180;
     bool block = 1;
     bool locked = true;
-public:
-    Lock(int power, int angle1, int angle2, bool block) {
-        this->power = power;
-        this->angle1 = angle1;
-        this->angle2 = angle2;
-        this->block = block;
-    };
-    Lock() {};
     bool open(bool open) {
+        hMot1.useAsServo();
         if(open) {
             printf("\rOpening...\n");
             hMot1.rotAbs(this->angle1, this->power, this->block);
@@ -27,16 +20,35 @@ public:
             printf("\rClosing...\n");
             hMot1.rotAbs(this->angle2, this->power, this->block);
         }
+        hMot1.releaseServo();
         this->locked = !open;
-        hMot1.stopRegulation(this->block, 10);
         printf("\rDone!\n");
         return open;
     };
+public:
+    Lock(int power, int angle1, int angle2) {
+        this->power = power;
+        this->angle1 = angle1;
+        this->angle2 = angle2;
+    };
+    Lock() {};
+    bool lock() {
+        return this->open(false);
+    }
+    bool unlock() {
+        return this->open(true);
+    }
+    bool switch() {
+        return this->open(!this->locked);
+    }
     bool isLocked() {
         return this->locked;
     }
     bool calibrate() {
         
+    }
+    bool free() {
+        return hMot1.stopRegulation(this->block, 10);
     }
 };
 
@@ -58,9 +70,9 @@ void hMain(void)
             if(!maindown) {
                 maindown = true;
                 if(open) {
-                    open = l.open(false);
+                    open = l.lock();
                 } else {
-                    open = l.open(true);
+                    open = l.unlock();
                 }
             }
         } else {
