1f50e95ca19da1ff61536f9df3f22d0a32cdb387
sirivan <vanyaturianitsa@gmail.com>
2016-05-11 10:38:50 +0200

    defined RFIDReader class

--- rfid.cpp
+++ rfid.cpp
@@ -4,52 +4,54 @@
 
 using namespace hFramework;
 
-void hMain(void)
-{
-    std::string buffer;
-    std::string tocomp;
-    tocomp = std::string(1, (char)0x02)+"110D04B3F5F2A448801859"+std::string(1, (char)0x03);
-	sys.setLogDev(&Serial);
-	hExt1.serial.init(57600, NONE, ONE);
-	hExt1.serial.selectSerial();
-	for (;;)
-	{
-	    while(hExt1.serial.available() > 0)
-        {
+class RFIDReader {
+private:
+    std::string buffer = "";
+    bool inRange = false;
+public:
+    RFIDReader() {
+        hExt1.serial.init(57600, NONE, ONE);
+        hExt1.serial.selectSerial();
+    };
+    bool available() {
+        while(hExt1.serial.available() > 0) {
             char c =  hExt1.serial.getch();
-            buffer += c;
+            this->buffer += c;
             if(c == (char)0x03){
                 //Parse
                 //Check length
                 //Check checksum
                 if(true){
-                    if(buffer.length() >= 10){
-                        if(!buffer.substr(3, 2).compare("0D")){
-                                if(buffer.substr((buffer.length()-5), 2) == "0A"){
-                                   LED1.off();
-                                }else{
-                                    if(!buffer.substr(5, (buffer.length()-10)).compare("04B3F5F2A44880")){
-                                    LED1.on();
+                    if(this->buffer.length() >= 10){
+                        if(!this->buffer.substr(3, 2).compare("0D")){
+                            if(this->buffer.substr((buffer.length()-5), 2) == "0A"){
+                                LED1.off();
+                            }else{
+                                LED1.on();
+                                if(!this->buffer.substr(5, (this->buffer.length()-10)).compare("04B3F5F2A44880")){
+                                    if(!this->inRange) {
+                                        this->inRange = true;
+                                        return true;
+                                    } else {
+                                        return false;
                                     }
-                                    //hExt1.serial.printf("\r\n4\r\n");
+                                } else {
+                                    this->inRange = false;
+                                    return false;
                                 }
+                                //hExt1.serial.printf("\r\n4\r\n");
+                            }
                         }
                     }
-                if(!buffer.compare(tocomp)){
-
-                   //hExt1.serial.printf("\r\nOK\r\n");
-                }else if(!buffer.compare(std::string(1, (char)0x02)+"110D0A4C"+std::string(1, (char)0x03))){
-                    //LED1.off();
-                }
                 }
                 //hExt1.serial.printf("\r\n%s\r\n", tocomp.c_str());
                 //hExt1.serial.printf("\r\n%s\r\n", buffer.c_str());
-                buffer = "";
+                this->buffer = "";
             }
             //110D6BB143D51209
         }
+    }
+};
 
-		hExt1.serial.printf("%c11090A41%c", (char)0x02, (char)0x03);
-		sys.delay_ms(100);
-	}
-}
+void hMain(void)
+{}
